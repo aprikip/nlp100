3aace14418620b5699daf3253fe8ff3a36bb6f55
Aprikip <suzu@a7m3.jp>
2012-04-20 11:29:30 +0900

    7本目をaddress.txtから読込む遣り方に変えた。

--- 007.rb
+++ 007.rb
@@ -1,4 +1,15 @@
 #!/usr/local/bin/ruby19
+# -*- coding: utf-8 -*-
 
-p open( "col1.txt" ).read.tr( "\n", "" ).split( "" ).uniq.size
+a = Array.new
 
+STDIN.read.split( "\n" ).each do |r|
+  a.push( r.split( "\t" )[0] )
+
+# ここで文字列に
+#   s += r.split( "\t" )[0] 
+# のように直接足し込んでいくととても時間がかかる。
+
+end
+
+p a.join( "" ).split( "" ).uniq.size
